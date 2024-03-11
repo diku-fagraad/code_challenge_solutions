a195fa26d0a4658ae58bbe4b5e0dfc2c2b0bdfb5
David JT <djt@hotmail.dk>
2024-03-11 01:32:50 +0100

    Add C# solution to CPR Number

---
+++ C#/CPRNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+int[] multipliers = { 4, 3, 2, 7, 6, 5, 4, 3, 2, 1 };
+
+string cpr = Console.ReadLine()!;
+cpr = string.Concat(cpr.AsSpan(0, 6), cpr.AsSpan(7, 4));
+
+int sum = 0;
+for (int i = 0; i < 10; i++)
+{
+	sum += int.Parse(cpr[i].ToString()) * multipliers[i];
+}
+int valid = sum % 11 == 0 ? 1 : 0;
+
+Console.WriteLine(valid);
